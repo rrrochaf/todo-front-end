4775131f9d314ad38a65b1f62839823871db637e
Ricardo Rocha <ricardorrf@gmail.com>
2022-02-23 08:28:37 -0300

    Route Guard Implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 import { TodoComponent } from './todo/todo.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  {path : '' , component : LoginComponent},
-  {path : 'welcome/:name' , component : WelcomeComponent},
+  {path : '' , component : LoginComponent}, // canActivate
+  {path : 'welcome/:name' , component : WelcomeComponent, canActivate : [RouteGuardService]},
   {path : 'login' , component : LoginComponent},
-  {path : 'todos' , component : ListTodosComponent},
+  {path : 'todos' , component : ListTodosComponent,canActivate : [RouteGuardService]},
   {path : 'logout' , component : LogoutComponent},
   {path : 'todo/:id' , component : TodoComponent},
   {path : 'convenio' , component : ConvenioComponent},
